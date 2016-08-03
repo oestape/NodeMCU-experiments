28cbad06b1f0f8abdf2d8d916783629b0fda132c
oestape <oestape@gmail.com>
2016-08-03 15:30:16 +1200

    Improvements in the css. Calling only one time to print to avoid multiple TCP messages.

--- NodeMCUWebServer/NodeMCUWebServer.ino
+++ NodeMCUWebServer/NodeMCUWebServer.ino
@@ -65,8 +65,6 @@ void loop() {
     return;
   }
 
-  client.setNoDelay(false);
-
   // Wait until the client sends some data
   Serial.println("new client");
   while (!client.available()) {
@@ -76,11 +74,11 @@ void loop() {
   // Read the first line of the request
   String request = client.readStringUntil('\r');
   Serial.println(request);
-  client.flush();
+  //client.flush();
 
   // Match the request
 
-int pos = 0;
+  int pos = 0;
   if (request.indexOf("/LED=ON") != -1)  {
     digitalWrite(ledPin, HIGH);
     ledValue = HIGH;
@@ -90,19 +88,16 @@ int pos = 0;
   } else if ((pos = request.indexOf("/SER=")) != -1) {
     //TODO
   } else if (request.indexOf("mystyle.css") != -1) {
-    client.println("HTTP/1.1 200 OK");
-    client.println("Content-Type: text/css");
-    client.println("Expires: Sat, 05 Apr 2025 00:00:00 GMT"); //we don't want to resend the css every time
-    client.println(""); //  do not forget this one
     printCSS(client);
     return;
 
   }
   else { //many browsers request favicon.ico for example
-    client.println("HTTP/1.1 404 Not Found");
-    client.println("Content-Type: text/html");
-    client.println(""); //  do not forget this one
-    client.println("-");
+    String out2 = String("");
+    out2 += "HTTP/1.1 404 Not Found\n";
+    out2 += "Content-Type: text/html\n";
+    out2 += "\n"; //  do not forget this one
+    out2 += "-\n";
 
     return;
   }
@@ -111,37 +106,41 @@ int pos = 0;
   //digitalWrite(ledPin, ledValue);
 
   // Return the response
-  client.println("HTTP/1.1 200 OK");
-  client.println("Content-Type: text/html");
-  client.println(""); //  do not forget this one
+  String out = String("");  //every time we do client.print, sends a TCP packet and waits for the response. With the String goes much faster (60ms instead of 400ms or enven 4s depending on the content)
+  out += "HTTP/1.1 200 OK\n";
+  out += "Content-Type: text/html\n";
+  out += "\n"; //  do not forget this one
 
 
-  client.print("<!DOCTYPE HTML>");
-  client.print("<html><head>");
-  //client.print("<style>body {zoom: 3}</style>");
+  out += "<!DOCTYPE HTML>";
+  out += "<html><head>";
+  //out += "<style>body {zoom: 3}</style>");
   //printCSS(client);
-  client.print("<link rel=\"stylesheet\" type=\"text/css\" href=\"mystyle.css\">");
-  client.print("</head><body>");
+  out += "<link rel=\"stylesheet\" type=\"text/css\" href=\"mystyle.css\">";
+  out += "</head><body>";
 
-  client.print("<br><br>");
-  client.print("<a href=\"/LED=ON\"");
-  if (ledValue == HIGH) client.print(" class=\"bp\"");
-  client.print(">ON</a> ");
-  client.print("<a href=\"/LED=OFF\"");
-  if (ledValue == LOW) client.print(" class=\"bp\"");
-  client.print(">OFF</a> ");
-  client.print("</body></html>");
+  out += "<br><br>";
+  out += "<a href=\"/LED=ON\"";
+  if (ledValue == HIGH) out += " class=\"bp\"";
+  out += ">ON</a> ";
+  out += "<a href=\"/LED=OFF\"";
+  if (ledValue == LOW) out += " class=\"bp\"";
+  out += ">OFF</a> ";
 
-  client.print("<br><br>");
+  out += "<br><br>";
   for (int i = 0; i < 9; i++)
   {
-    client.print("<a href=\"/SER=");
-    client.print((i - 4) * 45);
-    client.print("\"");
-    client.print(">");
-    client.print((i - 4) * 45);
-    client.print("</a> ");
+    out += "<a href=\"/SER=";
+    out += (i - 4) * 45;
+    out += "\"";
+    out += ">";
+    out += (i - 4) * 45;
+    out += "</a> ";
   }
+
+  out += "</body></html>";
+
+  client.print(out);
 
   delay(1);
   Serial.println("Client disonnected");
@@ -151,51 +150,58 @@ int pos = 0;
 
 void printCSS(WiFiClient client)
 {
-  //client.print("<style>");
+  String out = String("");
+  out += "HTTP/1.1 200 OK\n";
+  out += "Content-Type: text/css\n";
+  out += "Expires: Sat, 05 Apr 2025 00:00:00 GMT\n"; //we don't want to resend the css every time
+  out += "\n"; //  do not forget this one
 
-  client.print(""
-               //"body {zoom: 3}"
-               "a {"
-               "display: inline-block;"
-               "padding: 15px 25px;"
-               "font-size: 24px;"
-               "cursor: pointer;"
-               "text-align: center;"
-               "text-decoration: none;"
-               "outline: none;"
-               "color: #fff;"
-               "background-color: #4064bb;"
-               "border: none;"
-               "border-radius: 15px;"
-               "box-shadow: 0 9px #999;"
-               "font-family: Sans-Serif;"
-               "}"
+  //out += "<style>");
 
-               "a:hover {background-color: #2c4480}"
+  out += ""
+         //"body {zoom: 3}"
+         "a {"
+         "display: inline-block;"
+         "padding: 15px 25px;"
+         "font-size: 24px;"
+         "cursor: pointer;"
+         "text-align: center;"
+         "text-decoration: none;"
+         "outline: none;"
+         "color: #fff;"
+         "background-color: #4064bb;"
+         "border: none;"
+         "border-radius: 15px;"
+         "box-shadow: 0 9px #999;"
+         "font-family: Sans-Serif;"
+         "}"
 
-               "a:active {"
-               "background-color: #5a7ac7;"
-               "box-shadow: 0 0px #666;"
-               "transform: translateY(9px);"
-               "}"
+         "a:hover {background-color: #2c4480}"
 
-               ".bp {"
-               "color: #000;"
-               "background-color: #ffdb00;"
-               "box-shadow: 0 3px #999;"
-               "transform: translateY(6px);"
-               "}"
+         "a:active {"
+         "background-color: #5a7ac7;"
+         "box-shadow: 0 0px #666;"
+         "transform: translateY(9px);"
+         "}"
 
-               ".bp:hover {"
-               "background-color: #cbae00;"
-               "}"
-               ".bp:active {"
-               "background-color: #ffdb00;"
-               "box-shadow: 0 0px #666;"
-               "transform: translateY(9px);"
-               "}");
+         ".bp {"
+         "color: #000;"
+         "background-color: #ffdb00;"
+         "box-shadow: 0 3px #999;"
+         "transform: translateY(6px);"
+         "}"
+
+         ".bp:hover {"
+         "background-color: #cbae00;"
+         "}"
+         ".bp:active {"
+         "background-color: #ffdb00;"
+         "box-shadow: 0 0px #666;"
+         "transform: translateY(9px);"
+         "}";
 
 
-  //client.print("</style>");
+  //out += "</style>");
+  client.print(out);
 }
 
